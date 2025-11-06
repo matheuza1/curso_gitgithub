5467182a2d6c1650392b71db8035de29225815a1
matheuza1 <matheushenriquedelima614@gmail.com>
2025-11-06 18:02:40 -0300

    Mensagem com ano e o que estou cursando.

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -8,6 +8,7 @@ namespace MeuProjeto
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("O ano é 2025 e estou cursando Git e GitHub!");
         }
     }
 }
