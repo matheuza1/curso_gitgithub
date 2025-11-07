ed734e9b7a248bdb210c7bb79568df31413f705a
matheuza1 <matheushenriquedelima614@gmail.com>
2025-11-07 15:07:26 -0300

    tentativa de commitar

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -8,8 +8,8 @@ namespace MeuProjeto
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Causei um merge Conflict!");
-            Console.WriteLine("O ano é 2025 e estou cursando Git e GitHub!");
-            Console.WriteLine("Aqui vamos testar o Merge Conflict.");
+            Console.WriteLine("O ano é 2023 e estou cursando Git e GitHub!");
+            Console.WriteLine("Aqui vamos testar o Merge Conflict novamente.");
         }
     }
 }
