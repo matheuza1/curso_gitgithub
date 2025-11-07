acdb3e584772d231faebeebb3e17fadbd2e43b07
matheuza1 <matheushenriquedelima614@gmail.com>
2025-11-07 09:40:56 -0300

    Causando um merge conflict

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -9,6 +9,7 @@ namespace MeuProjeto
             Console.WriteLine("Hello World!");
             Console.WriteLine("Olá, Mundo!");
             Console.WriteLine("O ano é 2025 e estou cursando Git e GitHub!");
+            Console.WriteLine("Aqui vamos testar o Merge Conflict.");
         }
     }
 }
