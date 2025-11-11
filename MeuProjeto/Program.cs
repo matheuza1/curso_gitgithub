437e028c7e043f7238a2c44d1c62352aca3f1fd9
matheuza1 <matheushenriquedelima614@gmail.com>
2025-11-11 15:16:49 -0300

    Update Program.cs

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -11,15 +11,10 @@ namespace MeuProjeto
             Console.WriteLine("O ano é 2025 e estou cursando Git e GitHub!");
             Console.WriteLine("Aqui vamos testar o Merge Conflict novamente.");
 
-<<<<<<< Updated upstream
-            Console.WriteLine("Deu certo!");
-            Console.WriteLine("Funcionou!");
-=======
             Console.WriteLine("Como vou fazer para isso dar certo ?");
             Console.WriteLine("O que tem que ser feito para funcionar ?");
 
             Console.WriteLine("Merge com a branch Master");
->>>>>>> Stashed changes
         }
     }
 }
